36fc749fcb1a863d7b7f564f7463f485010f57d9
watcharap0n <wera.watcharapon@gmail.com>
2021-10-07 12:14:45 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
-const express = require('express')
-const consola = require('consola')
-const { loadNuxt } = require('nuxt-start')
-const app = express()
+const express = require('express');
+const consola = require('consola');
+const { Nuxt, Builder } = require('nuxt');
+const app = express();
 
-async function start () {
-  const nuxt = await loadNuxt(isDev ? 'dev' : 'start')
-  await nuxt.listen(process.env.PORT, process.env.HOST)
+const config = require('./nuxt.config.js');
+config.dev = process.env.NODE_ENV !== 'production';
+
+async function start() {
+  const nuxt = new Nuxt(config);
+  const { host, port } = nuxt.options.server;
+
+  if (config.dev) {
+    const builder = new Builder(nuxt);
+    await builder.build();
+  } else {
+    await nuxt.ready();
+  }
+
+  app.use(nuxt.render);
+
+  app.listen(port, host);
+  consola.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
+  });
 }
 
-start()
+start();
